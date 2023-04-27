cbc292259ecefc500731cde4c0893cc0a0932349
Forsen02 <129263203+a22ricni@users.noreply.github.com>
2023-04-27 10:23:47 +0200

    Made a intent
    I made a intent in the main activity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText nameEditView = findViewById(R.id.nameEditTextView);
+
+                Intent intent = new Intent(MainActivity.this, SubmitActivity.class);
+                intent.putExtra("name", nameEditView.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
