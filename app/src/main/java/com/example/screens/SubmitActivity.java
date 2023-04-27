f3632886d772707ca42f262ab2c5010e4fce2c2d
Forsen02 <129263203+a22ricni@users.noreply.github.com>
2023-04-27 09:35:40 +0200

    Added a activity
    I added the second activity.

---
+++ app/src/main/java/com/example/screens/SubmitActivity.java
@@ -0,0 +1,20 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+
+        Button submitButton = findViewById(R.id.button);
+
+        submit
+
+    }
+}
